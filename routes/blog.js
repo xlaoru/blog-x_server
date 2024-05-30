1f1fabe8d3ff7d4e7801affecb6a4e160de60289
xlaoru <sia212007@gmail.com>
2024-05-30 16:44:02 +0300

    v5.1

--- routes/blog.js
+++ routes/blog.js
@@ -13,11 +13,13 @@ const {
 
 router.get("/api/blogs", getBlogs);
 
-router.post("/api/blogs", sendBlog);
-// router.post("/api/blogs", isAuth, sendBlog); // Ready
+// ! router.post("/api/blogs", sendBlog);
+router.post("/api/blogs", isAuth, sendBlog); // * Ready
 
-router.put("/api/blogs/:id", updateBlog);
+// ! router.put("/api/blogs/:id", updateBlog);
+router.put("/api/blogs/:id", isAuth, updateBlog); // * Ready
 
-router.delete("/api/blogs/:id", deleteBlog);
+// ! router.delete("/api/blogs/:id", deleteBlog);
+router.delete("/api/blogs/:id", isAuth, deleteBlog); // * Ready
 
 module.exports = router;
